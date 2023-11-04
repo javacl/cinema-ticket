88a54fff738b349a92dd8626d427868c84128230
sadeghi <javacl.sadeghi@gmail.com>
2023-11-04 15:47:37 +0330

    #1 in this commit:  - created project  - added libs  - fixed manifest

--- app/src/main/java/sample/cinema/ticket/ui/theme/Color.kt
+++ app/src/main/java/sample/cinema/ticket/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package sample.cinema.ticket.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
